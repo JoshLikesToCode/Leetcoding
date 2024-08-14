76c9eb7dd1b420ccac638e9b81ab301e5c0a41bf
Joshua Stone <39971298+JoshLikesToCode@users.noreply.github.com>
2024-08-14 14:07:29 -0700

    Time: 384 ms (23.53%), Space: 46.6 MB (5.88%) - LeetHub

--- 0719-find-k-th-smallest-pair-distance/0719-find-k-th-smallest-pair-distance.cs
+++ 0719-find-k-th-smallest-pair-distance/0719-find-k-th-smallest-pair-distance.cs
@@ -1,15 +1,40 @@
+//            Brute Force Solution
+//  public class Solution {
+//     public int SmallestDistancePair(int[] nums, int k) {
+//         var list = new List<int>(nums.Length);
+//         list.AddRange(nums);
+//         for(var i = 0; i < nums.Length; i++)
+//         {
+//             for(var j = i + 1; j < nums.Length; j++)
+//             {
+//                 list.Add(Math.Abs(nums[i] - nums[j]));
+//             }
+//         }
+//         list.Sort();
+//         return list.ToArray()[k - 1];
+//     }
+// }
+
 public class Solution {
     public int SmallestDistancePair(int[] nums, int k) {
-        var list = new List<int>();
+        var max = Int32.MinValue;
+        foreach(var n in nums)
+            max = Math.Max(max, n);
+        var bucket = new int[max + 1];
         for(var i = 0; i < nums.Length; i++)
-        {
             for(var j = i + 1; j < nums.Length; j++)
             {
-                list.Add(Math.Abs(nums[i] - nums[j]));
+                var dist = Math.Abs(nums[i] - nums[j]);
+                bucket[dist]++;
             }
+        
+        // find kth smallest
+        for(var i = 0; i <= max; i++)
+        {
+            k -= bucket[i];
+            if(k <= 0)
+                return i;
         }
-        var arr = list.ToArray();
-        Array.Sort(arr);
-        return arr[k - 1];
+        return -1;
     }
 }
