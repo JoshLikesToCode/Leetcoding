51c460a6f427d221f4e2dcffd89d860052756dc1
Joshua Stone <39971298+JoshLikesToCode@users.noreply.github.com>
2024-08-18 10:36:36 -0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0598-range-addition-ii/0598-range-addition-ii.cs
@@ -0,0 +1,26 @@
+public class Solution {
+    public int MaxCount(int m, int n, int[][] ops) {
+        var matrix = new int[m][];
+        for(var i = 0; i < m; i++)
+            matrix[i] = new int[n];
+
+        foreach(var op in ops)
+        {
+            for(var i = 0; i < op[0]; i++)
+                for(var j = 0; j < op[1]; j++)
+                    matrix[i][j]++;
+        }
+        var max = Int32.MinValue;
+        for(var i = 0; i < m; i++)
+            for(var j = 0; j < n; j++)
+                max = Math.Max(max, matrix[i][j]);
+        var res = 0;
+        for(var i = 0; i < m; i++)
+            for(var j = 0; j < n; j++)
+                if(matrix[i][j] == max)
+                    res++;
+        return res;
+        
+        
+    }
+}
