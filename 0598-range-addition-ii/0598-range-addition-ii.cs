d0c07a0f2e604914b00f6ae9f4894b5172a777ef
Joshua Stone <39971298+JoshLikesToCode@users.noreply.github.com>
2024-08-18 10:37:16 -0700

    Time: 79 ms (43.9%), Space: 43.1 MB (75.61%) - LeetHub

--- 0598-range-addition-ii/0598-range-addition-ii.cs
+++ 0598-range-addition-ii/0598-range-addition-ii.cs
@@ -1,26 +1,37 @@
+/*        BRUTE FORCE SOLUTION                      */
+// public class Solution {
+//     public int MaxCount(int m, int n, int[][] ops) {
+//         var matrix = new int[m][];
+//         for(var i = 0; i < m; i++)
+//             matrix[i] = new int[n];
+
+//         foreach(var op in ops)
+//         {
+//             for(var i = 0; i < op[0]; i++)
+//                 for(var j = 0; j < op[1]; j++)
+//                     matrix[i][j]++;
+//         }
+//         var max = Int32.MinValue;
+//         for(var i = 0; i < m; i++)
+//             for(var j = 0; j < n; j++)
+//                 max = Math.Max(max, matrix[i][j]);
+//         var res = 0;
+//         for(var i = 0; i < m; i++)
+//             for(var j = 0; j < n; j++)
+//                 if(matrix[i][j] == max)
+//                     res++;
+//         return res;
+//     }
+// }
+
+
 public class Solution {
     public int MaxCount(int m, int n, int[][] ops) {
-        var matrix = new int[m][];
-        for(var i = 0; i < m; i++)
-            matrix[i] = new int[n];
-
         foreach(var op in ops)
         {
-            for(var i = 0; i < op[0]; i++)
-                for(var j = 0; j < op[1]; j++)
-                    matrix[i][j]++;
+            m = Math.Min(m, op[0]);
+            n = Math.Min(n, op[1]);
         }
-        var max = Int32.MinValue;
-        for(var i = 0; i < m; i++)
-            for(var j = 0; j < n; j++)
-                max = Math.Max(max, matrix[i][j]);
-        var res = 0;
-        for(var i = 0; i < m; i++)
-            for(var j = 0; j < n; j++)
-                if(matrix[i][j] == max)
-                    res++;
-        return res;
-        
-        
+        return m * n;
     }
 }
