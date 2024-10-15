8db6924e159ecaf0ad50b92a37cb795319fe9448
Joshua Stone <39971298+JoshLikesToCode@users.noreply.github.com>
2024-10-15 15:37:56 -0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 1154-day-of-the-year/1154-day-of-the-year.cs
@@ -0,0 +1,8 @@
+public class Solution {
+    public int DayOfYear(string date) {
+        var parsedDate = DateTime.Parse(date, System.Globalization.CultureInfo.InvariantCulture);
+        var dateYear = int.Parse(date.Split('-')[0]);
+        var lastDate = new DateTime(dateYear, 12, 31);
+        return DateTime.IsLeapYear(dateYear) ? 366 : 365 - (lastDate - parsedDate).Days;
+    }
+}
