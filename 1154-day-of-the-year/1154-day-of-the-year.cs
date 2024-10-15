b2c1f084dfb54335efa9ad201f3d109d0a8c2794
Joshua Stone <39971298+JoshLikesToCode@users.noreply.github.com>
2024-10-15 15:38:53 -0700

    Time: 88 ms (24.73%), Space: 72.7 MB (23.66%) - LeetHub

--- 1154-day-of-the-year/1154-day-of-the-year.cs
+++ 1154-day-of-the-year/1154-day-of-the-year.cs
@@ -3,6 +3,6 @@ public class Solution {
         var parsedDate = DateTime.Parse(date, System.Globalization.CultureInfo.InvariantCulture);
         var dateYear = int.Parse(date.Split('-')[0]);
         var lastDate = new DateTime(dateYear, 12, 31);
-        return DateTime.IsLeapYear(dateYear) ? 366 : 365 - (lastDate - parsedDate).Days;
+        return (DateTime.IsLeapYear(dateYear) ? 366 : 365) - (lastDate - parsedDate).Days;
     }
 }
