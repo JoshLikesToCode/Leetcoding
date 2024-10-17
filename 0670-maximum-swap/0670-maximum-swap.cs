596ec0931e89a1bed5958826325266f3b2bcb934
Joshua Stone <39971298+JoshLikesToCode@users.noreply.github.com>
2024-10-17 10:25:07 -0700

    Time: 0 ms (100%), Space: 27.2 MB (35.5%) - LeetHub

--- 0670-maximum-swap/0670-maximum-swap.cs
+++ 0670-maximum-swap/0670-maximum-swap.cs
@@ -1,21 +1,20 @@
 public class Solution {
     public int MaximumSwap(int num) {
         var strNum = num.ToString();
-        var listOfIntsFromNum = new List<int>();
+        int max = num;
         for(var i = 0; i < strNum.Length; i++)
-            listOfIntsFromNum.Add(strNum[i] - '0');
-        var maxNum = listOfIntsFromNum.Max();
-        var idxOfMax = listOfIntsFromNum.IndexOf(maxNum);
-        if(idxOfMax != 0)
-            strNum = Swap(0, idxOfMax, strNum.ToCharArray());
-        return Convert.ToInt32(strNum);
-    }
-
-    public string Swap(int a, int b, char[] arr)
-    {
-        var tmp = arr[a];
-        arr[a] = arr[b];
-        arr[b] = tmp;
-        return new string(arr);
+        {
+            for(var j = i + 1; j < strNum.Length; j++)
+            {
+                var charArr = strNum.ToCharArray();
+                var tmp = charArr[i];
+                charArr[i] = charArr[j];
+                charArr[j] = tmp;
+                
+                var tmpNum = Convert.ToInt32(new string(charArr));
+                max = Math.Max(max, tmpNum);
+            }
+        }
+        return max;
     }
 }
