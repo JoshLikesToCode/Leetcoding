58e6b0e94661cad3e7fd8917edc5e28d36468c8e
Joshua Stone <39971298+JoshLikesToCode@users.noreply.github.com>
2024-10-20 00:42:59 -0700

    Time: 0 ms (100%), Space: 39 MB (100%) - LeetHub

---
+++ 1784-check-if-binary-string-has-at-most-one-segment-of-ones/1784-check-if-binary-string-has-at-most-one-segment-of-ones.cs
@@ -0,0 +1,15 @@
+public class Solution {
+    public bool CheckOnesSegment(string s) {
+        bool found = false, segment = false;
+        foreach(var c in s)
+        {
+            if(c == '1')
+                found = true;
+            else if(c == '0' && found)
+                segment = true;
+            if(c is '1' && segment)
+                return false;
+        }
+        return true;
+    }
+}
