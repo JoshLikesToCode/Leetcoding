5e21f4482e189246205a0a8119d617eafb692be4
Joshua Stone <39971298+JoshLikesToCode@users.noreply.github.com>
2024-10-27 17:26:13 -0700

    Time: 5 ms (50%), Space: 41 MB (5.71%) - LeetHub

--- 2496-maximum-value-of-a-string-in-an-array/2496-maximum-value-of-a-string-in-an-array.cs
+++ 2496-maximum-value-of-a-string-in-an-array/2496-maximum-value-of-a-string-in-an-array.cs
@@ -2,10 +2,7 @@ public class Solution {
     public int MaximumValue(string[] strs) {
         var list = new List<int>();
         foreach(var s in strs)
-        {
-            Console.WriteLine($"s = {s} and CalculateValue(s) = {CalculateValue(s)}");
             list.Add(CalculateValue(s));
-        }
         return list.Max();
     }
 
