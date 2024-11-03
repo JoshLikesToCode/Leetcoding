fc4b02982dcdc1472736a6709df6bde959068e05
Joshua Stone <39971298+JoshLikesToCode@users.noreply.github.com>
2024-11-02 23:48:38 -0700

    Time: 0 ms (100%), Space: 39.1 MB (84.35%) - LeetHub

--- 0796-rotate-string/0796-rotate-string.cs
+++ 0796-rotate-string/0796-rotate-string.cs
@@ -2,7 +2,6 @@ public class Solution {
     public bool RotateString(string s, string goal) {
         if(s.Length != goal.Length)
             return false;
-        var d = s + s;
-        return d.Contains(goal);
+        return (s+s).Contains(goal);
     }
 }
