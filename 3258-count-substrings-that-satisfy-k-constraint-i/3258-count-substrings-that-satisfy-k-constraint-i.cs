31fd2d3b6f237e798a047b49e974b53a1a589065
Joshua Stone <39971298+JoshLikesToCode@users.noreply.github.com>
2024-11-06 18:31:32 -0800

    Time: 40 ms (33.33%), Space: 77.7 MB (13.24%) - LeetHub

---
+++ 3258-count-substrings-that-satisfy-k-constraint-i/3258-count-substrings-that-satisfy-k-constraint-i.cs
@@ -0,0 +1,27 @@
+public class Solution {
+    public int CountKConstraintSubstrings(string s, int k) {
+        
+        var substrings = new List<string>(){s};
+        for (int length = 1; length < s.Length; length++)
+        {
+            for (int start = 0; start <= s.Length - length; start++)
+            {
+            var substring = s.Substring(start, length);
+            substrings.Add(substring);
+            }
+        }
+        var count = 0;
+        foreach(var substring in substrings)
+        {
+            var map = new int[2];
+            foreach(var c in substring)
+                if(c == '0')
+                    map[0]++;
+                else
+                    map[1]++;
+            if(map[0] <= k || map[1] <= k)
+                count++;
+        }
+        return count;
+    }
+}
