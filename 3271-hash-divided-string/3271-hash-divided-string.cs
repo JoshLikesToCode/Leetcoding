b55e1bc72b32f5e05a9368b7a84ebc51b7e32379
Joshua Stone <39971298+JoshLikesToCode@users.noreply.github.com>
2024-11-12 17:58:14 -0800

    Time: 3 ms (100%), Space: 46.5 MB (56.94%) - LeetHub

--- 3271-hash-divided-string/3271-hash-divided-string.cs
+++ 3271-hash-divided-string/3271-hash-divided-string.cs
@@ -1,42 +1,18 @@
 public class Solution {
     public string StringHash(string s, int k) {
-        if(k == 1)
-            return s;
-        var substrings = new List<string>(){s.Length == k ? s : null};
-        substrings.RemoveAll(x => x == null);
-        var res = string.Empty;
-        for (int length = 1; length < s.Length; length++)
-            for (int start = 0; start <= s.Length - length; start+=2)
-            {
-                var substring = s.Substring(start, length);
-                if(substring.Length % k == 0)
-                    substrings.Add(substring);
-            }
-        foreach(var str in substrings)
-            res += GetHashValue(str);
-        return res;
-    }
+        var result = string.Empty;
+        int sum = 0, idx = 0;
 
-    private char GetHashValue(string val)
-    {
-        int x = 0, sum = 0;
-        for(var i = 0; i < val.Length; i++)
-        {
-            for(var start = 'a'; start <= 'z'; start++)
-            {
-                if(start == val[i])
-                {
-                    sum += x;
-                    x = 0;
-                    break;
-                }
-                x++;
+        for (int i = 0; i < s.Length; i++) {
+            idx++;
+            sum += s[i] - 'a';
+            if(idx == k) {
+                idx = 0;
+                sum %= 26;
+                result += (char)('a' + sum);
+                sum = 0;
             }
         }
-        var res = 'a';
-        var count = 0;
-        while(++count <= (sum%26))
-            res++;
-        return res;
+        return result;
     }
 }
