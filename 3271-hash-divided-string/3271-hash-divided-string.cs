64ee1c05af8f96dc0ef98bff249fe022b87037ce
Joshua Stone <39971298+JoshLikesToCode@users.noreply.github.com>
2024-11-12 17:59:18 -0800

    Time: 3 ms (100%), Space: 46.7 MB (44.44%) - LeetHub

--- 3271-hash-divided-string/3271-hash-divided-string.cs
+++ 3271-hash-divided-string/3271-hash-divided-string.cs
@@ -1,18 +1,20 @@
 public class Solution {
     public string StringHash(string s, int k) {
-        var result = string.Empty;
+        var res = string.Empty;
         int sum = 0, idx = 0;
 
-        for (int i = 0; i < s.Length; i++) {
+        for (int i = 0; i < s.Length; i++) 
+        {
             idx++;
             sum += s[i] - 'a';
-            if(idx == k) {
+            if(idx == k) 
+            {
                 idx = 0;
                 sum %= 26;
-                result += (char)('a' + sum);
+                res += (char)('a' + sum);
                 sum = 0;
             }
         }
-        return result;
+        return res;
     }
 }
