b710e3a839b23199d6b75b9e91f879631ebc05a2
Joshua Stone <39971298+JoshLikesToCode@users.noreply.github.com>
2024-12-16 18:59:32 -0800

    Time: 808 ms (5.03%), Space: 47.3 MB (15.83%) - LeetHub

---
+++ 0135-candy/0135-candy.cs
@@ -0,0 +1,25 @@
+public class Solution {
+    public int Candy(int[] ratings) {
+        var candies = new int[ratings.Length];
+        Array.Fill(candies, 1);
+        var hasChanged = true;
+        while(hasChanged)
+        {
+            hasChanged = false;
+            for(int i = 0; i < ratings.Length; i++)
+            {
+                if(i != ratings.Length - 1 && ratings[i] > ratings[i+1] && candies[i] <= candies[i+1])
+                {
+                    candies[i] = candies[i+1] + 1;
+                    hasChanged = true;
+                }
+                if(i > 0 && ratings[i] > ratings[i-1] && candies[i] <= candies[i-1])
+                {
+                    candies[i] = candies[i-1] + 1;
+                    hasChanged = true;
+                }
+            }
+        }
+        return candies.Sum();
+    }
+}
