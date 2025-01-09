a6ff2c60379098a6410a6c6df344bd86a60965df
Joshua Stone <39971298+JoshLikesToCode@users.noreply.github.com>
2025-01-09 12:08:27 -0800

    Time: 9 ms (19.06%), Space: 43.6 MB (29.91%) - LeetHub

---
+++ 3309-count-prefix-and-suffix-pairs-i/3309-count-prefix-and-suffix-pairs-i.cs
@@ -0,0 +1,22 @@
+public class Solution {
+    public int CountPrefixSuffixPairs(string[] words) {
+        var res = 0;
+        for(var i = 0; i < words.Length; i++)
+        {
+            var word1 = words[i];
+            for(var j = i + 1; j < words.Length; j++)
+            {
+                var word2 = words[j];
+                res += IsPrefixAndSuffix(word1, word2) ? 1 : 0;
+            }
+        }
+        return res;
+    }
+
+    public bool IsPrefixAndSuffix(string str1, string str2)
+    {
+        var prefix = str2.StartsWith(str1);
+        var suffix = str2.EndsWith(str1);
+        return prefix && suffix;
+    }
+}
