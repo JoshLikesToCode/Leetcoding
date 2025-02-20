d67d6757d042ff9756791bf2473726dc21090b98
Joshua Stone <39971298+JoshLikesToCode@users.noreply.github.com>
2025-02-19 16:16:05 -0800

    Time: 2 ms (30.77%), Space: 42.8 MB (34.62%) - LeetHub

---
+++ 1980-find-unique-binary-string/1980-find-unique-binary-string.cs
@@ -0,0 +1,16 @@
+public class Solution {
+    public string FindDifferentBinaryString(string[] nums) {
+        var hs = new HashSet<int>();
+        foreach(var n in nums)
+            hs.Add(Convert.ToInt32(n, 2));
+        for(var n = 0; n <= nums.Length; n++)
+            if(!hs.Contains(n))
+            {
+                var res = Convert.ToString(n, 2).ToString();
+                while(res.Length < nums.Length)
+                    res = "0" + res;
+                return res;
+            }
+        return "";
+    }
+}
